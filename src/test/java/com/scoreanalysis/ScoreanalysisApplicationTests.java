d9f51f983ad945f82a15313f4d6d46909e0767c0
Starry-Hu <1756487384@qq.com>
2019-06-17 12:28:02 +0800

    测试发送邮件功能成功

--- src/test/java/com/scoreanalysis/ScoreanalysisApplicationTests.java
+++ src/test/java/com/scoreanalysis/ScoreanalysisApplicationTests.java
@@ -2,15 +2,28 @@ package com.scoreanalysis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ScoreanalysisApplicationTests {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     @Test
-    public void contextLoads() {
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("1756487384@qq.com");
+        message.setTo("1191539143@qq.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容,杨江臭狗屎");
+
+        mailSender.send(message);
     }
 
 }
